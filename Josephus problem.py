62d7f820b6e02a2fb685d7fc78f86080c5a1b787
edErikson <ed.d.erikson@gmail.com>
2024-04-21 14:32:27 +0300

    josephus recursion

--- Josephus problem.py
+++ Josephus problem.py
@@ -11,3 +11,15 @@ def josephus(n, k):
 
 
 print(josephus(23, 6))
+
+
+def josephus2(n, k):
+    if n == 1:
+        return 1
+    return (josephus(n - 1, k) + k - 1) % n + 1
+
+
+# Example:
+n = 23  # Number of individuals
+k = 6  # Elimination interval
+print("Last survivor's position:", josephus(n, k))
